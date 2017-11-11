968610166b59d337b89b0568eb2b2d60ec1d6137
ma3100 <masato.syuukatu@gmail.com>
2017-11-11 15:16:34 +0900

    初回コミット

---
+++ MessagePackCSharpStudy/MessagePackCSharpStudy/Program.cs
@@ -0,0 +1,20 @@
+﻿using MessagePack;
+using MessagePackCSharpStudy.Tutorial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePackCSharpStudy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var basicSerialize = new BasicSerialize();
+			basicSerialize.DoSerialize();
+			
+		}
+	}
+}
---
+++ MessagePackCSharpStudy/MessagePackCSharpStudy/Tutorial/BasicSerialize.cs
@@ -0,0 +1,70 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagePackCSharpStudy.Tutorial
+{
+	[MessagePackObject]
+	public class Sample1
+	{
+		[Key(0)]
+		public int Foo { get; set; }
+		[Key(1)]
+		public int Bar { get; set; }
+	}
+
+	[MessagePackObject]
+	public class Sample2
+	{
+		[Key("foo")]
+		public int Foo { get; set; }
+		[Key("bar")]
+		public int Bar { get; set; }
+	}
+
+	[MessagePackObject(keyAsPropertyName: true)]
+	public class Sample3
+	{
+		// no needs KeyAttribute
+		public int Foo { get; set; }
+
+		// If ignore public member, you can use IgnoreMemberAttribute
+		[IgnoreMember]
+		public int Bar { get; set; }
+	}
+
+	// 番外編 普通書こうと思わないけれど
+	[MessagePackObject]
+	public class IntKeySample
+	{
+		[Key(3)]
+		public int A { get; set; }
+		[Key(10)]
+		public int B { get; set; }
+	}
+
+
+	public class BasicSerialize
+	{
+		public void DoSerialize()
+		{
+			// [10,20]
+			Console.WriteLine(MessagePackSerializer.ToJson(new Sample1 { Foo = 10, Bar = 20 }));
+
+			// {"foo":10,"bar":20}
+			Console.WriteLine(MessagePackSerializer.ToJson(new Sample2 { Foo = 10, Bar = 20 }));
+
+			// {"Foo":10}
+			// 文字列の方がコストかかるけれど、色がつく
+			Console.WriteLine(MessagePackSerializer.ToJson(new Sample3 { Foo = 10, Bar = 20 }));
+
+			// 番外編 キーで指定した個所に保持されてしまう。
+			// [null,null,null,0,null,null,null,null,null,null,0]
+			Console.WriteLine(MessagePackSerializer.ToJson(new IntKeySample()));
+
+		}
+	}
+}
